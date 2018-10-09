d83db516c82302e80e75038aaf83478f74588410
Shunichiro Ida <shunichiroid@gmail.com>
2018-10-09 13:28:02 +0900

    Migrate lure model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_013047) do
+ActiveRecord::Schema.define(version: 2018_10_09_024015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lures", force: :cascade do |t|
+    t.string "type"
+    t.string "color"
+    t.float "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
