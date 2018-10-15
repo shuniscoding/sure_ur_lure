5b5ea0b014d723c3561ff533a70f3cb0312b140a
Shunichiro Ida <shunichiroid@gmail.com>
2018-10-15 18:25:18 +0900

    Add color and weight data genarator

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,55 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# 色のデータの生成
+colors = [
+  {
+    name: '赤'
+  },
+  {
+    name: '青'
+  },
+  {
+    name: '緑'
+  },
+  {
+    name: '黄'
+  },
+  {
+    name: 'オレンジ'
+  },
+  {
+    name: 'ピンク'
+  },
+  {
+    name: '白'
+  },
+  {
+    name: '黒'
+  }
+]
+Color.create!(colors) { |c| puts c.name }
+
+# おもさのデータ生成
+def weight_data_generator(i)
+  while(i <= 4.1)
+      Weight.create!(
+        milligram: i.to_s
+      )
+      i += 0.1
+      puts Weight.last
+  end
+end
+
+weight_data_generator(0.1)
+
 # 天気のデータを生成
 weathers = [{ name: '晴れ' }, { name: '曇り' }, { name: '雨' }]
 Weather.create!(weahters) { |w| puts w.name}
 
 # 釣り場のデータを生成
-locations = [{
+locations = [
+  {
     name: 'としまえんフィッシングエリア'
   },
   {
@@ -44,6 +87,7 @@ Location.create!(locations) { |l| puts l.name}
     email: Faker::Internet.safe_email,
     password: Faker::Internet.password
   )
+  puts User.last
 end
 puts "Created #{User.count} users"
 
